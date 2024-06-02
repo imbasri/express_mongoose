315be8c7db8c991249250209c0698b9953b130af
imbasri <imbasri.dev@gmail.com>
2024-06-03 03:08:52 +0700

    get products by id
    mengambil data berdasarkan id dari productnya.

--- index.js
+++ index.js
@@ -3,27 +3,40 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-
-
-
 // Models 
 
-const Product = require('./models/product')
-
+const Product = require("./models/product");
 
 // connect to mongodb
-mongoose.connect('mongodb://127.0.0.1:27017/shop_db').then((_res) => {
-  console.log('connected to db')
-}).catch((err) => { console.log(err) })
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shop_db")
+  .then((_res) => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+// connect to mongodb
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shop_db")
+  .then((_res) => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //router
-app.get('/', (_req, res) => {
-  res.send('Hello World')
-})
+app.get("/", (_req, res) => {
+  res.send("Hello World");
+});
 
 app.get('/products', async (_req, res) => {
   const products = await Product.find({})
@@ -31,10 +44,15 @@ app.get('/products', async (_req, res) => {
   res.render('products/index', { products })
 })
 
-
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findById(id)
+  res.render('products/show', { product })
+})
 
 // listen server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 })
+
