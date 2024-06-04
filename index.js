0a8e97b9630ea5a9d4c5a76cc709b5b9b1875d63
imbasri <imbasri.dev@gmail.com>
2024-06-04 11:22:32 +0700

    update dan delete

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Models 
+// Models
 
 const Product = require("./models/product");
 
@@ -20,6 +21,7 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 // connect to mongodb
 mongoose
   .connect("mongodb://127.0.0.1:27017/shop_db")
@@ -30,39 +32,55 @@ mongoose
     console.log(err);
   });
 
-
 //router
 app.get("/", (_req, res) => {
   res.send("Hello World");
 });
 // get all products
-app.get('/products', async (_req, res) => {
-  const products = await Product.find({})
+app.get("/products", async (_req, res) => {
+  const products = await Product.find({});
   // console.log(products)
-  res.render('products/index', { products })
-})
+  res.render("products/index", { products });
+});
 
 // create products
-app.get('/products/create', (_req, res) => {
-  res.render('products/create')
-})
+app.get("/products/create", (_req, res) => {
+  res.render("products/create");
+});
 
 // create products
-app.post('/products', async (req, res) => {
-  const product = new Product(req.body)
-  await product.save()
-  res.redirect(`/products/${product._id}`)
-})
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
 // get product by id
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params
-  const product = await Product.findById(id)
-  res.render('products/show', { product })
-})
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
+// render update product by id
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
 
+// update product by id
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+  res.redirect(`/products/${product._id}`);
+});
+// delete product by id
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
 // listen server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
-})
-
+});
